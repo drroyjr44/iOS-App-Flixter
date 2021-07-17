2941f3657249992f8221bec4c0288f280f6bf918
Arnob Roy <roy.arnob4@outlook.com>
2021-07-16 23:58:02 -0500

    Uploadinding second week Flix progress

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Arnob Roy on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
